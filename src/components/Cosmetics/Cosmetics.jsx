49cc824c38d488bea2c3a04e5b86ae5bcbd20bd2
SM Sohag <sohagsheik32@gmail.com>
2023-03-24 20:33:53 +0600

    update

---
+++ src/components/Cosmetics/Cosmetics.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Cosmetics = () => {
+  return (
+    <div className="">
+      <h1 className="text-center">Welcome to Cosmetics shop</h1>
+    </div>
+  );
+};
+
+export default Cosmetics;
